ef6b33673d14c98245ae9daa7613b3a9d6e2cc07
Semie Rogers <redrogers@gmail.com>
2018-02-22 12:22:39 -0600

    Adds JsonToTreePipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Tree, TreeNode, TreeModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { JsonToTreePipe } from './json-to-tree.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonToTreePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/json-to-tree.pipe.ts
@@ -0,0 +1,82 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TreeNode } from 'primeng/primeng';
+
+/* Takes any json object and transforms it to
+ * json formatted for use in PrimeNg Tree.
+ * Expanded and collapsed icons can be customized with FontAwesome 4 icons.
+ * 
+ * JSON after formatting looks like this:
+ * {"label": "value",
+ *  "children": [{"label": "value"}, {"label": "value"}]
+ * }
+ */
+
+@Pipe({name: 'jsonToTree'})
+export class JsonToTreePipe implements PipeTransform {
+    transform(json: any): TreeNode[] {
+
+        const isPlainObject = ((obj) => {
+          if (!Array.isArray(obj) && obj !== null && typeof (obj) === 'object') {
+            return true;
+          }
+          return false;
+        });
+    
+        const isNil = ((val) => {
+          return (val === undefined || val === null || val.length <= 0) ? true : false;
+        });
+    
+        const handleChild = ((array, label) => {
+          return array.map((element, index) => {
+            return { 'label': label + ' #' + (index + 1), 'styleClass': 'highlight-off', 'children': createTreeNodes(element) };
+          });
+        });
+    
+        const handleStringArray = ((stringArray) => {
+          return stringArray.map(string => {
+            return { 'label': string };
+          });
+        });
+       
+    
+        const createTreeNodes = ((obj) => {
+          const objArray = [];
+          if (isPlainObject(obj)) {
+            const objectEntries = Object.entries(obj);
+            objectEntries.forEach((objectEntry) => {
+              if (Array.isArray(objectEntry[1]) && isPlainObject(objectEntry[1][0])) {
+                objArray.push({
+                  'label': objectEntry[0], 'styleClass': 'highlight-off', 'data': '""',
+                  'children': handleChild(objectEntry[1], objectEntry[0])
+                });
+              } else if (Array.isArray(objectEntry[1]) && Array.isArray(objectEntry[1][0])) {
+                objArray.push({ 'label': objectEntry[0], 'data': '', 'children': handleChild(objectEntry[1][0], objectEntry[0]) });
+              } else if (Array.isArray(objectEntry[1]) && typeof objectEntry[1][0] !== 'object') {
+                objArray.push({ 'label': objectEntry[0], 'styleClass': 'highlight-off', 'children': handleStringArray(objectEntry[1]) });
+              } else if (isPlainObject(objectEntry[1])) {
+                objArray.push({
+                  'label': objectEntry[0], 'styleClass': 'highlight-off',
+                  'children': createTreeNodes(objectEntry[1])
+                });
+              } else {
+                objArray.push({ 'label': objectEntry[0] + ' : ' + objectEntry[1], 'data': objectEntry[0] });
+              }
+            });
+          } else if (Array.isArray(obj)) {
+            return obj.map((arrayItem) => {
+              handleChild(arrayItem, '');
+            });
+          } else {
+            return obj;
+          }
+          return objArray;
+        });
+    
+    if (isNil(json)) {
+        return undefined;
+    }
+    return createTreeNodes(json);
+
+    }
+}
+
