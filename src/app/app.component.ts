9fa73daf6358e2dc249516ca375ccf63f5fe7ab6
Semie Rogers <redrogers@gmail.com>
2018-02-23 18:09:39 -0600

    Demo code piped to page. Start of primeng tree

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { JsonPipe } from '@angular/common';
+import { JsonToTreePipe } from './json-to-tree.pipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  tree = false;
+  json: any = 
+    {
+      "key": "value",
+      "array": ["array item 1", "array item 2", "array item 3"],
+      "object": {
+          "nested key 1": "nested value 1",
+          "nested key 2": "nested key 2",
+          "nested key 3": "nested key 3"
+      },
+      "object array":  [
+        {"object array key 1": "object array value 1"},
+        {"object array key 2": "object array value 2"},
+        {"object array key 3": "object array value 3"},
+        {"nested array in object array": ["nested array item 1", "nested array item 2", "nested array item 3"]}
+      ]
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Tree, TreeNode, TreeModule } from 'primeng/primeng';
+import { Tree, TreeNode, TreeModule, ButtonModule } from 'primeng/primeng';
+import { PrettyJsonModule } from 'angular2-prettyjson';
 
 import { AppComponent } from './app.component';
 import { JsonToTreePipe } from './json-to-tree.pipe';
+
 
 
 @NgModule({
@@ -13,9 +15,10 @@ import { JsonToTreePipe } from './json-to-tree.pipe';
   ],
   imports: [
     BrowserModule,
-    TreeModule
+    TreeModule,
+    ButtonModule,
+    PrettyJsonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
