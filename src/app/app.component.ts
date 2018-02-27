2ce34f4667d2b1267843401716a2e771bbae10e0
Semie Rogers <redrogers@gmail.com>
2018-02-26 19:13:57 -0600

    Add sidebar, change json demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { JsonToTreePipe } from './json-to-tree.pipe';
-import { TreeNode, Tree} from 'primeng/primeng';
+import { TreeNode, Tree, Sidebar} from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +11,21 @@ export class AppComponent {
   tree = false;
   json: any = 
     {
-      "key": "value",
-      "array": ["array item 1", "array item 2", "array item 3"],
-      "object": {
+      "Object": "value",
+      "Array": ["array item 1", "array item 2", "array item 3"],
+      "Nested Object": {
           "nested key 1": "nested value 1",
           "nested key 2": "nested key 2",
           "nested key 3": "nested key 3"
       },
-      "object array":  [
+      "Object Array":  [
         {"object array key 1": "object array value 1"},
         {"object array key 2": "object array value 2"},
         {"object array key 3": "object array value 3"},
-        {"nested array in object array": ["nested array item 1", "nested array item 2", "nested array item 3"]}
+        {"Nested Array in Object Array": ["nested array item 1", "nested array item 2", "nested array item 3"]}
       ]
     }
+  
+    
 }
 
